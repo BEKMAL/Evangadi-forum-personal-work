dedecb56bf151384b1809a0c9f93543937b7f74a
BEKAML <bekalumsc@gmail.com>
2024-10-10 12:41:10 +0300

    Home jsx updated for username display

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -4,9 +4,8 @@ const dbConnection = mysql2.createConnection({
     // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock", //path to mysql sock in MAMP
     user:process.env.user,
     database:process.env.database,
-    host:" sql12.freemysqlhosting.net",
-    password:process.env.password
-   
+    host:"sql12.freemysqlhosting.net",
+    password:process.env.password,
    
   });
   module.exports=dbConnection.promise()
