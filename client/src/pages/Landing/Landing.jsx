86116798428d048a68775ff78d8df7eb86862f90
BEKAML <bekalumsc@gmail.com>
2024-10-10 16:16:30 +0300

    show/hide passowrd functionality addede

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Login from '../../Components/Login/Login'
-
+import bgnew from '../../Images/bgnew.png'
 import classes from './landing.module.css'
 import About from '../../Components/About/About'
 function Landing() {
   return (
-       <div className={classes.Landing__flex}>
+       <div className={classes.Landing__flex} style={{backgroundImage:`url(${bgnew})`}}>
               <Login/>
               <About/>
      </div> 
